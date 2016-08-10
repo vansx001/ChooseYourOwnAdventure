4bd691b904ab2596a3e5d1cf7ea478ce7f6e0730
vansx001 <vansx001@gmail.com>
2016-08-10 14:23:12 -0500

    Added Class to add "reader name"

---
+++ Choose Your Own Adventure - Game/Choose Your Own Adventure/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Choose_Your_Own_Adventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Story story = new Story();
+            story.RunStory();
+        }
+    }
+}
---
+++ Choose Your Own Adventure - Game/Choose Your Own Adventure/Reader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Choose_Your_Own_Adventure
+{
+    class Reader
+    {
+        string name;
+
+        public Reader()
+        {
+        }
+        public Reader(string name)
+        {
+            this.name = name;
+        }
+    public string getName()
+        {
+            Console.WriteLine("What is your name? \n");
+            this.name = Console.ReadLine();
+            return this.name;
+        }
+    }
+}
